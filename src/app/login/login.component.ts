7107f148a648163e890003025f2fea7b05fc24b6
Pauli Winkelmann <paw@ti8m.ch>
2017-10-25 13:40:27 +0200

    login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login',  // <login></login>
+  styleUrls: [ './login.component.scss' ],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+}
